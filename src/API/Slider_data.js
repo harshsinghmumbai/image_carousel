66df516ee836d570ba969fde83932689b7fa4a1c
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-03-18 14:16:13 +0530

    completed html and css part and also fully responsive

---
+++ src/API/Slider_data.js
@@ -0,0 +1,22 @@
+const Slider_data = [
+  {
+    url: "https://images.unsplash.com/photo-1709929170176-bc35e8bd57b1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1709525091854-7152bddb6d9d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1709874662602-139e1114da11?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1709746837880-f96b4f588ce5?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1709395866199-e1edf84690bd?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1709395866199-e1edf84690bd?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+export default Slider_data;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import Slider_data from "./API/Slider_data";
+import { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
 const App = () => {
-  return <></>;
+  const [data, setdata] = useState(Slider_data);
+  return (
+    <>
+      <div
+        id="Parent_container"
+        className="max-w-[1400px] m-auto w-full h-[400px] px-5 py-16 border-2 border-black "
+      >
+        <div
+          id="image_container"
+          style={{ backgroundImage: `url(${data[0].url})` }}
+          className="border w-full h-full bg-center bg-cover rounded-xl relative"
+        >
+          <IoIosArrowBack
+            id="left_arrow"
+            className="absolute top-[50%] left-3 text-2xl text-white bg-black/20 rounded-full p-1"
+          />
+          <IoIosArrowForward
+            id="right_arrow"
+            className="absolute top-[50%] right-3 text-2xl text-white bg-black/20 rounded-full p-1"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
